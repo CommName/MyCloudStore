a77da5f60aae9ba927f18d715265e3aa3dcb1370
CommName <CommName@users.noreply.github.com>
2019-12-05 00:15:02 +0100

    user and files class'

---
+++ MyCloudStore/Service/App_Code/UserFiles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for UserFiles
+/// </summary>
+public class UserFiles
+{
+   public int ID { get; set; }
+    public string fileName { get; set; }
+    public string hash { get; set; }
+
+}
