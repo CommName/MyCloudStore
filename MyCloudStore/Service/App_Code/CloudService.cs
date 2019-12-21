e3a906ead40a8006aa32093d667e580ee3119113
CommName <CommName@users.noreply.github.com>
2019-12-21 04:56:24 +0100

    Extended file transfer size

--- MyCloudStore/Service/App_Code/CloudService.cs
+++ MyCloudStore/Service/App_Code/CloudService.cs
@@ -8,7 +8,7 @@ using System.Text;
 // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CloudService" in code, svc and config file together.
 public class CloudService : ICloudService
 {
-    public static uint chunkSize = 32; //U kb
+    public static uint chunkSize = 2*1024; //U kb
     public void createNewFile(string username, byte[] password, string fileName, byte[] fileHash)
     {
         UserContainer.Instance.createNewFile(username, password, fileName, fileHash);
